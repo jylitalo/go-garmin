a0e3c60bff36f2337e478a805aa65718021a13f3
Juha Ylitalo <juha@ylitalot.net>
2024-12-24 23:10:02 +0200

    [client] Fix typo and output error message

--- client.go
+++ client.go
@@ -176,7 +176,9 @@ func (c *Client) apiGet(out any, path string, params url.Values) error {
 	}
 	defer res.Body.Close()
 	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("recived bad status code %q", res.Status)
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(res.Body)
+		return fmt.Errorf("received bad status code: %d, body: %s", res.StatusCode, string(buf.Bytes()))
 	}
 	return json.NewDecoder(res.Body).Decode(out)
 }
